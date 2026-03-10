70bc18552f1f514bd9c43a1dcd319695733dcf1b
umg-git <myocv@miumg.edu.gt>
2026-03-09 21:01:38 -0600

    Codigo para validacion

--- interpreter.py
+++ interpreter.py
@@ -9,6 +9,28 @@ class Interpreter(LanguageVisitor):
     # program: 'program' '{' (declaration | statement)* '}' ;
     def visitProgram(self, ctx: LanguageParser.ProgramContext):
         return self.visitChildren(ctx)
+            # conditional: 'if' '(' condition ')' block ('else' block)? ;
+    def visitConditional(self, ctx: LanguageParser.ConditionalContext):
+        condition_result = self.visit(ctx.condition())
+        if condition_result:
+            return self.visit(ctx.block(0))
+        elif ctx.block(1):  # Si existe el bloque else
+            return self.visit(ctx.block(1))
+        return None
+
+    # condition: left=expr op=('>' | '<' | '==' | '!=' | '>=' | '<=') right=expr ;
+    def visitCondition(self, ctx: LanguageParser.ConditionContext):
+        left = self.visit(ctx.left)
+        right = self.visit(ctx.right)
+        op = ctx.op.text
+
+        if op == '>':  return left > right
+        if op == '<':  return left < right
+        if op == '==': return left == right
+        if op == '!=': return left != right
+        if op == '>=': return left >= right
+        if op == '<=': return left <= right
+        return False
 
     # expr: left=expr op=('*'|'/') right=expr # MulDiv
     def visitMulDiv(self, ctx: LanguageParser.MulDivContext):
